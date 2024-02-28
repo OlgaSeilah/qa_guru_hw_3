28ef66f0a8c553812f9213b7449f22f0cf0f48b0
seilah <princenoru@gmail.com>
2024-02-28 22:53:22 +0400

    javaFaker: enums added

---
+++ src/test/java/pageObjects/testData/enums/Genders.java
@@ -0,0 +1,17 @@
+package pageObjects.testData.enums;
+
+public enum Genders {
+    FEMALE("Female"),
+    MALE("Male"),
+    OTHER("Other");
+
+    private String gender;
+
+    Genders(String gender) {
+        this.gender = gender;
+    }
+
+    public String getValue() {
+        return this.gender;
+    }
+}
